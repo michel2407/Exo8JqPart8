68aecb9ee7fbc418328f8a21a425119d2c6dcfb7
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-11 11:22:10 +0200

    Correction de l'exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// Je cible son bouton masqué grâce à son id. et Je luis rajoute un évènement au clique.
+//Au clique sur mon bouton, j'affiche mon texte qui l'id 'text'
 $(function(){
   $('#hide').click(function(){
     $('#text').hide();
   })
-})
+// Je cible son bouton affiché grâce à son id. et Je luis rajoute un évènement au clique.
+//Au clique sur mon bouton, j'affiche mon texte qui l'id 'text'
 $(function(){
   $('#show').click(function(){
     $('#text').show();
